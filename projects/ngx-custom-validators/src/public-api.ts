cb1ddf85dced18cd4606b527a3af13e78e201160
Yaser <yaser.94ae@gmail.com>
2020-05-20 21:45:41 +0200

    delete unused files

--- projects/ngx-custom-validators/src/public-api.ts
+++ projects/ngx-custom-validators/src/public-api.ts
@@ -1,7 +1,4 @@
 /*
  * Public API Surface of ngx-custom-validators
  */
-
-export * from './lib/ngx-custom-validators.service';
-export * from './lib/ngx-custom-validators.component';
 export * from './lib/ngx-custom-validators.module';
