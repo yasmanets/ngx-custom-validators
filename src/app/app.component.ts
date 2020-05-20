e0c123a1698383f338a6f50420f3247cd4d19f0f
Yaser <yaser.94ae@gmail.com>
2020-05-20 21:47:36 +0200

    test form with an email input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { NgxCustomValidators } from './../../projects/ngx-custom-validators/src/lib/ngx-custom-validators.module';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'custom-validators';
+export class AppComponent implements OnInit {
+
+  public title: string;
+  public testForm: FormGroup;
+
+  constructor() {
+    this.title = 'ngx-custom-validators';
+  }
+
+  ngOnInit() {
+    this.setTestForm();
+  }
+
+  private setTestForm(): void {
+    this.testForm = new FormGroup({
+      email: new FormControl(null, {
+        validators: NgxCustomValidators.emailValidator({email: true}),
+        updateOn: 'blur',
+      }),
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule
   ],
   providers: [],
